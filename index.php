fd4bb0dc8d60f3e5a307d1e85a634f12216ca598
unknown <maxim_91@inbox.ru>
2019-07-23 16:46:16 +0300

    DZ-0 complete

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * My script
+ * PHP version 5
+ *
+ * @category Class
+ * @package  PackageName
+ * @author   Maxim Ivanov <maxim@inbox.ru>
+ * @license  https://google.com PHP license
+ * @link     https://google.com
+ */
+
+const TESTCONSTANTA = 'asd';
+$user_name = "Igor";
+
+function show_something()
+{
+    return 'SOMETHING';
+}
+
+if (1==1) {
+    echo 'hi';
+}
