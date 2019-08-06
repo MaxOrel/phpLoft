0860e159ddebce72c19ec2daa652707d8982f1bd
unknown <maxim_91@inbox.ru>
2019-08-06 17:41:59 +0300

    dz3 complete

--- dz3/index.php
+++ dz3/index.php
@@ -1,23 +1,31 @@
 <?php
 require('src/functions.php');
 
-//printOrderDelivery();
+const POST_URL = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json';
+
+printOrderDelivery();
 
 $testArray = [
-  'users' => [
-    [
-      'name' => 'Mike',
-      'id' => 1
-    ],
-    [
-      'name' => 'Venom',
-      'id' => 2
-    ],
-    [
-      'name' => 'Metrandir',
-      'id' => 3,
-      'properties' => ['city' => 'SPb']
+    'users' => [
+        [
+            'name' => 'Mike',
+            'id' => 1
+        ],
+        [
+            'name' => 'Venom',
+            'id' => 2
+        ],
+        [
+            'name' => 'Metrandir',
+            'id' => 3,
+            'properties' => ['city' => 'SPb']
+        ]
     ]
-  ]
 ];
 arrayToJson($testArray);
+
+
+saveCsvArray(randomArray(50));
+getCsvSumm('arrayCSV.csv');
+
+parserPage(POST_URL);
